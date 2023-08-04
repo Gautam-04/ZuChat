7f85e2d16a9236b6a44dc1c659ea8f57d96775b3
Gautam-666 <2022.gautam.rai@ves.ac.in>
2023-08-04 20:41:42 +0530

    Added few new files
    Signed-off-by: Gautam-666 <2022.gautam.rai@ves.ac.in>

--- src/components/sidebar/SideBar.jsx
+++ src/components/sidebar/SideBar.jsx
@@ -1,10 +1,16 @@
 import './Sidebar.scss';
+import contact from '../../utils/sample'
 
 import React from 'react'
 
 export default function SideBar() {
   return (
-    <div className='SideBar'>SideBar</div>
-  )
+    <div className="Sidebar">
+      <div className="items">
+        <h1>Gautam Rai</h1>
+        <p>raigautam2004</p>
+      </div>
+    </div>
+  );
 }
 
---
+++ src/utils/sample.js
@@ -0,0 +1,29 @@
+const contact = [
+  {
+    id: 0,
+    name: "Gautam Rai",
+    Email: "raigautam2004@gmail.com",
+  },
+  {
+    id: 1,
+    name: "Gautam Rai",
+    Email: "raigautam2004@gmail.com",
+  },
+  {
+    id: 2,
+    name: "Gautam Rai",
+    Email: "raigautam2004@gmail.com",
+  },
+  {
+    id: 3,
+    name: "Gautam Rai",
+    Email: "raigautam2004@gmail.com",
+  },
+  {
+    id: 4,
+    name: "Gautam Rai",
+    Email: "raigautam2004@gmail.com",
+  },
+];
+
+export default contact;
